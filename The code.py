d48ad07853412b119f477211cc690d7372c036a5
SuperSid99 <62835595+SuperSid99@users.noreply.github.com>
2021-09-27 04:58:27 +0530

    Update The code.py

--- The code.py
+++ The code.py
@@ -1,154 +1,143 @@
-# The code creates the datasets and performs isolation forest to detect outliers
-#functions
-
-# to generate datapoints
-
-def generate_datapoint(r,bias):
-    data_point=[]
-    for i in range(128):
-        z=random.randint(1,r)
-        if z>=bias:
-            bit=1
-        else:
-            bit=0
-        b=[bit]
-        data_point.append(b)
-    return(data_point)
+# functions
 
 # to get inputs for X_test
 def get_test_samples():
-    sample = input('\nenter the sample:    ')
-    count=0
-    for _ in sample:
-        if _ =="1" or "0":
-            count+=1
-        else:
-            print ("\nplease enter a valid sample")
-            get_test_samples()
-    if count == 128:
-        return(sample)
+    Sample = input("\nenter the sample:    ")
+    if re.search("^[0-1]{128}$", Sample):
+        return Sample
     else:
-        print ("\nplease enter a valid sample")
-        get_test_samples()
+        print("\nplease enter a valid sample")
+        return get_test_samples()
 
 
+# any 128 bit number
+# input is a string of 128 char containing only 0 or 1
+# output is an array of 32 arrays with float values between 0.0 and 1.5
+def convolute_ip_test(a):
+    Count = 0
+    ConvolutedIp = []
+    ConvolutedIpElement = ""
+    for _ in a:
+        Count += 1
+        if Count == 4:
+            ConvolutedIpElement += _
+            ConvolutedIp.append(int(ConvolutedIpElement, 2) / 10)
+            ConvolutedIpElement = ""
+            Count = 0
+        else:
+            ConvolutedIpElement += _
+
+    return ConvolutedIp
 
 
-#any 128 bit number
+# any 128 bit number
 # input is an array of 128 arrays with each sub array containing 0 or 1 for the ip
 # output is an array of 32 arrays with float values between 0.0 and 1.5
 def convolute_ip(a):
-    y=[]
-    count=0
-    z=""
+    Count = 0
+    ConvolutedIp = []
+    ConvolutedIpElement = ""
     for _ in a:
-        _=_.strip('[')
-        _=_.strip(']')
-        count+=1
-        if count == 4:
-            z+=str(_[0])
-            y.append(int(z,2)/10)
-            z=""
-            count=0
+        _ = _.strip("[")
+        _ = _.strip("]")
+        Count += 1
+        if Count == 4:
+            ConvolutedIpElement += str(_[0])
+            ConvolutedIp.append(int(ConvolutedIpElement, 2) / 10)
+            ConvolutedIpElement = ""
+            Count = 0
         else:
-            z+=_
+            ConvolutedIpElement += _
 
-    return(y)
+    return ConvolutedIp
 
 
-
-#to predict if an ip is outlier
-def pedict_outlier(predictor,X):
-    z=predictor.predict(X)
-    if z==1:
-        return('null')
+# to predict if an ip is outlier
+def pedict_outlier(Predictor, X):
+    Prediction = Predictor.predict(X)
+    if Prediction == 1:
+        return "null"
     else:
-        return(X)
+        return X
 
 
-#main Structure
+# main Structure
+if __name__ == "__main__":
 
-#---Imoprting Libraries---
+    # ---Imoprting Libraries---
 
-import numpy as np
-import pandas as pd
-import re
-import csv
+    import numpy as np
+    import pandas as pd
+    import re
+    import csv
+    from sklearn.ensemble import IsolationForest
 
-from sklearn.ensemble import IsolationForest
+    # ---Training Phase---
 
-#---Creating Datasets---
+    Dataset = pd.read_csv(input("\n Enter the name of the dataset"))
+    X_train = Dataset.iloc[:, :].values
 
-with open('ip_data_train.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    for _ in range(10000):
-        writer.writerow(generate_datapoint(4,2))
+    X_test = []
+    Outliers = []
+    X_train_convoluted = []
+    CompareTest = []
 
-with open('ip_data_test.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    for _ in range(100):
-        writer.writerow(generate_datapoint(4,4))
-    
-    for _ in range(100):
-        writer.writerow(generate_datapoint(3,3))
-    
-    for _ in range(100):
-        writer.writerow(generate_datapoint(2,2))
+    for _ in X_train:
+        X_train_convoluted.append(convolute_ip(_))
 
-    for _ in range(100):
-        writer.writerow(generate_datapoint(3,2))
+    # contamination = 0 - 0.5 -- expected outlier to total ratio
+    # np.random.RandomState(42) -- to get replicable results
+    Predictor = IsolationForest(
+        max_samples=100, contamination=0.01, random_state=np.random.RandomState(42)
+    )
 
+    Predictor.fit(X_train_convoluted)
 
+    # ---Test Phase---
 
-#---Training Phase---
+    TypeTest = input("\nDo you want to use a dataset or add samples? (d or s)")
+    if TypeTest.lower()[0] == "s":
+        while True:
+            MoreSamples = input("\nDo you want to add samples? (yes or no)")
+            if MoreSamples.lower()[0] == "y":
+                X_test.append(get_test_samples())
+            elif MoreSamples.lower()[0] == "n":
+                break
+            else:
+                print("\nenter a valid responce")
 
+        for _ in X_test:
+            ConvolutedIpElement = convolute_ip_test(_)
+            Prediction = pedict_outlier(Predictor, [ConvolutedIpElement])
+            if type(Prediction) == str:
+                Outliers.append(Prediction)
+            else:
+                Outliers.append(_)
 
-rng = np.random.RandomState(42) # to get replicable results
-dataset = pd.read_csv("ip_data_train.csv")
-X_train=dataset.iloc[:, :].values
+        for _ in range(len(X_test)):
+            CompareTest.append((X_test[_], Outliers[_]))
 
-dataset_test = pd.read_csv("ip_data_test.csv")
-X_test=dataset_test.iloc[:, :].values
+        print(CompareTest)
 
-outliers=[]
-X_train_convoluted=[]
-X_test_convoluted=[]
-compare_test=[]
-
-
-for a in X_train:
-    X_train_convoluted.append(convolute_ip(a))
-
-for a in X_test:
-    X_test_convoluted.append(convolute_ip(a))
-
-
-# contamination = 0 - 0.5 -- expected outlier to total ratio
-
-predictor = IsolationForest(max_samples=100, contamination = 0.01, random_state=rng)
-
-predictor.fit(X_train_convoluted)
-
-#---Test Phase---
-
-
-for _ in X_test_convoluted:
-    X=[]
-    Y=[]
-    for i in _:
-        X.append(i)
-    Y.append(X)
-    z = pedict_outlier(predictor,Y)
-    if type(z)==str:
-        outliers.append(z)
     else:
-        outliers.append('anomaly')
+        DatasetTest = pd.read_csv(input("\n Enter the name of the dataset"))
+        X_test = DatasetTest.iloc[:, :].values
+        X_test_convoluted = []
 
-for _ in range(len(X_test_convoluted)):
-    compare_test.append((_+1,outliers[_]))
+        for a in X_test:
+            X_test_convoluted.append(convolute_ip(a))
 
-print(compare_test)
+        for _ in X_test_convoluted:
+            ConvolutedIpElement = []
+            for i in _:
+                ConvolutedIpElement.append(i)
+            Prediction = pedict_outlier(Predictor, [ConvolutedIpElement])
+            if type(Prediction) == str:
+                Outliers.append(z)
+            else:
+                Outliers.append("anomaly")
 
+        for _ in range(len(X_test_convoluted)):
+            CompareTest.append((_ + 1, Outliers[_]))
 
+        print(CompareTest)
